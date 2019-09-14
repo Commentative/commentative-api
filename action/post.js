8c2504ce478791bb1147d39aefea4d3e86fa8562
Jamie Brown <jamiebrown201@hotmail.com>
2019-09-14 22:26:26 +0100

    add working get

---
+++ action/post.js
@@ -0,0 +1,49 @@
+"use strict";
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.main = async event => {
+  try {
+    const { commentData, articleBody } = JSON.parse(event.body);
+
+    if (false) {
+      // Validate data
+    }
+
+    const timestamp = new Date().getTime();
+
+    const params = {
+      TableName: process.env.COMMENTATIVE_TABLE,
+      Item: {
+        uuid: uuid(),
+        articleBody,
+        comments: [
+          {
+            ...commentData,
+            createdAt: timestamp,
+            updatedAt: timestamp,
+            uuid: uuid()
+          }
+        ],
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
+    };
+
+    const result = await dynamoDb.put(params).promise();
+
+    if (false) {
+      // Check for error
+    }
+
+    console.log(result);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+  } catch (err) {
+    console.error(err);
+  }
+};
